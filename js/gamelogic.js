476b1686718839059097902630dbcda9b0db4012
Matt Hammonde <mhammonde@users.noreply.github.com>
2018-05-23 17:00:31 +0100

    Update gamelogic.js

--- js/gamelogic.js
+++ js/gamelogic.js
@@ -1,5 +1,7 @@
-var canvas = document.getElementById("gameWindow");
-var ctx = canvas.getContext("2d");
-ctx.font = "18px Calibri";
-ctx.fillStyle="#000";
-ctx.fillText("Hello World",10,50);
+function runGame() {
+  var canvas = document.getElementById("gameWindow");
+  var ctx = canvas.getContext("2d");
+  ctx.font = "18px Calibri";
+  ctx.fillStyle="#000";
+  ctx.fillText("Hello World",10,50);
+}
