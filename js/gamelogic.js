10f32ec67c002052f4466547b7495c06a53282d6
Matt Hammonde <mhammonde@users.noreply.github.com>
2018-05-23 17:06:09 +0100

    Update gamelogic.js

--- js/gamelogic.js
+++ js/gamelogic.js
@@ -1,7 +1,7 @@
 function runGame() {
   var canvas = document.getElementById("gameWindow");
   var ctx = canvas.getContext("2d");
-  ctx.font = "18px Calibri";
-  ctx.fillStyle="#000";
-  ctx.fillText("Hello World",10,50);
+  ctx.font = "16px Calibri";
+  ctx.fillStyle="#fff";
+  ctx.fillText("The Dark. A text adventure by Falsehand. Copyright 2018 Falsehand Games. All rights reserved.",10,50);
 }
