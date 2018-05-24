4811d02d3aaf55b9e9c7649d8ff507526ad9bc52
Matt Hammonde <mhammonde@users.noreply.github.com>
2018-05-24 10:28:59 +0100

    Update gamelogic.js

--- js/gamelogic.js
+++ js/gamelogic.js
@@ -6,7 +6,7 @@ function runGame() {
                     "As you come to, you suddenly realise you are not in your own bed.",
                     "You sit bolt upright, immediately noticing the roughness of the ground you are laying on.",
                     "You take a moment and realise that whilst you eyes are open, there is nothing but darkness."
-                   
+                  ];
   ctx.font = "normal normal 12px Calibri";
   ctx.fillStyle= "#fff";
   ctx.textAlign = "left";
