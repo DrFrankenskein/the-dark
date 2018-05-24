a63f08adafe6954f36e85b81385e9ec0dbf191e4
Matt Hammonde <mhammonde@users.noreply.github.com>
2018-05-24 11:08:16 +0100

    Update gamelogic.js

--- js/gamelogic.js
+++ js/gamelogic.js
@@ -8,13 +8,15 @@ function runGame() {
         "You sit bolt upright, noticing you are on the ground.",
         "Your eyes are open, but all you see is black."
     ];
-
+    ctx.width = 640;
+    ctx.height = 480;
+    
     ctx.font = "normal normal 12px Calibri";
     ctx.fillStyle= "#fff";
     ctx.textAlign = "left";
 
     for (i = 0; i < introText.length+1; i++) {
-        console.log("introText index[" + i + "] - " + introText[i])
-        ctx.fillText(introText[i],5,lineHeight*i);
+        //console.log("introText index[" + i + "] - " + introText[i])
+        ctx.fillText(introText[i], 5, lineHeight * i);
     }
 }
