17381d193113075ca73e78522e18dab90214d285
Matt Hammonde <mhammonde@users.noreply.github.com>
2018-05-24 10:54:07 +0100

    Update gamelogic.js

--- js/gamelogic.js
+++ js/gamelogic.js
@@ -1,17 +1,19 @@
 function runGame() {
-  var canvas = document.getElementById("gameWindow");
-  var ctx = canvas.getContext("2d");
-  var lineHeight = 30;
-  var introText = [ "You wake up.",
-                    "As you come to, you suddenly realise you are not in your own bed.",
-                    "You sit bolt upright, immediately noticing the roughness of the ground you are laying on.",
-                    "You take a moment and realise that whilst you eyes are open, there is nothing but darkness."
-                  ];
-  ctx.font = "normal normal 12px Calibri";
-  ctx.fillStyle= "#fff";
-  ctx.textAlign = "left";
-  
-  for (i = 0; i =< introText.length; i++) {
-                   ctx.fillText(introText[i],5,lineHeight*introText);
-  }
+    var canvas = document.getElementById("gameWindow");
+    var ctx = canvas.getContext("2d");
+    var lineHeight = 30;
+    var introText = [
+        "You wake up.",
+        "You realise you are not in your own bed.",
+        "You sit bolt upright, noticing you are on the ground.",
+        "Your eyes are open, but all you see is black."
+    ];
+
+    ctx.font = "normal normal 12px Calibri";
+    ctx.fillStyle= "#fff";
+    ctx.textAlign = "left";
+
+    for (i = 0; i <= introText.length; i++) {
+        ctx.fillText(introText[i],5,lineHeight*introText);
+    }
 }
