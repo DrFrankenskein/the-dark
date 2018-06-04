9434d9e5731af59bb37ec7be2b7b0860071b3627
Matt Hammonde <mhammonde@users.noreply.github.com>
2018-06-04 09:51:43 +0100

    Major logic overhaul and separation of dialog

---
+++ js/dialog.js
@@ -0,0 +1,7 @@
+var dlgIntro = [
+	"*Blink*, *Blink*",
+	"You wake up...",
+	"You open your eyes, but all you see is darkness."
+];
+
+var locations = ["Start","Forward 1"]
--- js/gamelogic.js
+++ js/gamelogic.js
@@ -1,64 +1,95 @@
-var canvas = document.getElementById("gameWindow");
-var ctx = canvas.getContext("2d");
-var FPS = 5;
-var timer = 0;
-var dialog = [
-        "You wake up.",
-        "You realise you are not in your own bed.",
-        "You sit bolt upright, noticing you are on the ground.",
-        "Your eyes are open, but all you see is black."
-    ];
+var gameWindow = document.getElementById("gameWindow"),
+	dialogWindow = document.getElementById("dialog"),
+	c2c = document.getElementById("c2c"),
+	intro = false,
+	actions = ["fwd","bwd","left","right","look","touch","take","drop","throw","use","ask","tell"],
+	btns = document.getElementsByTagName("button"),
+	loc = 0;
 
-setInterval(function() {
-    update();
-    draw();
-    console.log("FRAME");
-}, 1000/FPS);
-
-function update() {
-    timer =+ 1;
-    dispDialog(timer);
+for (i = 0; i < btns.length; i++) {
+	var button = btns[i];
+	button.addEventListener("click", createButtons);
 }
 
-function draw() {
-    ctx.font = '14pt Calibri';
-    ctx.fillStyle = '#CCC';
-    
-    //timer
-    ctx.fillText(timer, 5, 600);
-    
-    //button - DIRECTIONS
-    ctx.strokeStyle = '#CCC';
-    ctx.strokeRect(5, 350, 122, 90);
-    ctx.fillText('F', 10, 355);        
-    ctx.fillText('B', 10, 355);
-    ctx.fillText('L', 5, 355);
-    ctx.fillText('R', 15, 355);
-        
-    //button - LOOK        
-    ctx.strokeStyle = '#CCC';
-    ctx.strokeRect(132, 350, 122, 40);
-    ctx.fillText('BACKWARD', 7, 355);        
-    
-    //button - TOUCH                
-    ctx.strokeStyle = '#CCC';
-    ctx.strokeRect(264, 350, 122, 40);        
-    ctx.fillText('LEFT', 7, 355);
-
-    //button - PICKUP                
-    //button - DROP
-    //button - THROW
-    //button - USE
-    //button - ASK
-    //button - TELL
-             
+function createButtons() {
+	usrAction(this.value);
 }
 
-function dispDialog(frame) {
-    var dSpeed = frame/100;
-    var lineHeight = 20;
-    ctx.font = "normal normal 14pt Calibri";
-    ctx.fillStyle= "#fff";
-    ctx.textAlign = "left";
-    ctx.fillText(dialog[dSpeed], 5, lineHeight * dSpeed);
+function usrAction(action) {
+	switch (action) {
+		case "fwd":
+			dispDialog("You get up and step forward...",0);
+			loc =+ 1;
+			alert(locations[loc]);
+			break;
+		case "bwd":
+			dispDialog("You step back...",0);
+			loc =- 1;
+			alert(locations[loc]);
+			break;
+		case "left":
+			dispDialog("You try to move...",0);
+			break;
+		case "right":
+			dispDialog("You try to move...",0);
+			break;
+		case "look":
+			dispDialog("You try to look around, but all you see is darkness.",0);
+			break;
+		case "touch":
+			dispDialog("You feel the gritty ground under your fingers.",0);
+			break;
+		case "take":
+			dispDialog("There is nothing here to take.",0);
+			break;
+		case "drop":
+			dispDialog("You are not holding anything.",0);
+			break;
+		case "throw":
+			dispDialog("You are not holding anything.",0);
+			break;
+		case "use":
+			dispDialog("Use what?",0);
+			break;
+		case "ask":
+			dispDialog("You call out, but there is no reply.",0);
+			break;
+		case "tell":
+			dispDialog("You call out, but there is no reply.",0);
+			break;		
+	}			
 }
+
+function slowEach(array,interval,callback) {
+    if(!array.length) return;
+    var i = 0;
+    next();
+    function next() {
+        if(callback(array[i],i) !== false) {
+            if( ++i < array.length ) {
+                setTimeout(next,interval);
+            }
+        }
+    }
+}
+	
+function dispDialog(text) {
+	dialogWindow.innerHTML = text;
+}
+
+function introduction() {
+	slowEach(dlgIntro, 4000, function(element,index) {
+		dialogWindow.classList.add("animFadeIn");
+		dispDialog(element);
+	});
+	
+	ctrlWindow.classList.add("animFadeIn");
+}
+
+function startGame() {
+	if (intro) {
+	introduction();
+	}
+}
+
+startGame();
